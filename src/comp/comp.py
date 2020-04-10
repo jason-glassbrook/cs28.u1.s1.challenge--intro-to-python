3166df1f31bed8659d66c723cb74545b082979ce
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-10 12:33:56 -0400

    comp.py: complete g

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -67,8 +67,8 @@ print(f)
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
-print("All names uppercase:")
-g = []
+print("All names uppercase, 5 added to ages:")
+g = [Human(human.name.upper(), human.age + 5) for human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
